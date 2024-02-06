9e584cfbe1ca1fe4a1d895ebae295755dab5c521
serg444s <143083342+serg444s@users.noreply.github.com>
2024-02-06 07:58:33 +0000

    Deploying to gh-pages from @ serg444s/goit-js-hw-11@6ad41caf9ed5fa888e705599a79c7f4cfbe64fe2 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,12 @@
-import{S as c,i as l}from"./assets/vendor-7659544d.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const a={form:document.querySelector(".user-form"),formInput:document.querySelector(".form-inpu"),btn:document.querySelector(".form-button"),galleryList:document.querySelector(".gallery")};function u(n){const t=`https://pixabay.com/api?key=42220995-e7901b62efa710cae16c4a0a7&q=${n}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(t).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()})}function f(n){const o=n.map(t=>`<li class="gallery-item">
-  <a class="gallery-link" href="${t.largeImageURL}">
-    <img class="gallery-image" src="${t.webformatURL}" alt="${t.tags}" />
+import{S as c,i as l}from"./assets/vendor-7659544d.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=s(t);fetch(t.href,r)}})();const a={form:document.querySelector(".user-form"),formInput:document.querySelector(".form-inpu"),btn:document.querySelector(".form-button"),galleryList:document.querySelector(".gallery")};function u(e){const s=`https://pixabay.com/api?key=42220995-e7901b62efa710cae16c4a0a7&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(s).then(n=>{if(!n.ok)throw new Error(n.status);return n.json()})}function f(e){return`<li class="gallery-item">
+  <a class="gallery-link" href="${e.largeImageURL}">
+    <img class="gallery-image" src="${e.webformatURL}" alt="${e.tags}" />
   </a>
   <div class="list-text">
-  <p>Likes <span>${t.likes}</span></p>
-<p>Views <span>${t.views}</span></p>
-<p>Comments <span>${t.comments}</span></p>
-<p>Downloads <span>${t.downloads}</span></p>
+  <p>Likes <span>${e.likes}</span></p>
+<p>Views <span>${e.views}</span></p>
+<p>Comments <span>${e.comments}</span></p>
+<p>Downloads <span>${e.downloads}</span></p>
 </div>
-</li>`).join("");a.galleryList.innerHTML=o}new c(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});const p="Sorry, there are no images matching your search query. Please try again!";function m(){l.error({message:p,position:"topRight"})}function d(n){n.preventDefault();const o=n.currentTarget.elements.input.value.trim();console.log(o),u(o).then(f).catch(m).finally(a.form.requestFullscreen())}a.form.addEventListener("submit",d);
+</li>`}function p(e){const o=e.map(f).join("");a.galleryList.innerHTML=o}new c(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});const m="Sorry, there are no images matching your search query. Please try again!";function d(){l.error({message:m,position:"topRight"})}function y(e){e.preventDefault();const o=e.currentTarget.elements.input.value.trim();console.log(o),u(o).then(p).catch(d).finally(a.form.reset())}a.form.addEventListener("submit",y);
 //# sourceMappingURL=commonHelpers.js.map
