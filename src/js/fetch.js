c9f372322975011fad533ff7792e815f64b6595c
serg444s <143083342+serg444s@users.noreply.github.com>
2024-02-06 14:58:12 +0200

    Update fetch.js

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -2,7 +2,7 @@ import { refs } from './refs';
 import { onError } from './onError';
 
 export function fetchImg(input) {
-  const URL = `https://pixabay.com/api?key=42220995-e7901b62efa710cae16c4a0a7&q=${input}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const URL = `https://pixabay.com/api/?key=42220995-e7901b62efa710cae16c4a0a7&q=${input}&image_type=photo&orientation=horizontal&safesearch=true`;
   return fetch(URL).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
